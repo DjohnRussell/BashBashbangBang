2cccbe3cd305fb2edb301f8fc2b6e0d46ab151c4
Daniel John Russell <89018934+DjohnRussell@users.noreply.github.com>
2024-03-23 14:33:53 +0100

    Update bashButler.sh

--- bashButler.sh
+++ bashButler.sh
@@ -48,12 +48,13 @@ case $num in
 			1)
 				clear
 				echo "Linux"
-				echo "name of the software"
+				echo Name of the software"
 				read softwareName
 				clear
 				sudo apt-get install $softwareName
 				;;
 			2)
+			clear
 				echo "Mac"
 				echo "name of the software"
 				read softwareName
